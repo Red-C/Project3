6c9f2c9dbe750015c5e1146834917765dce6331c
Hongfei <hong25100@hotmail.com>
2015-03-14 15:59:00 -0700

    testScript modified

--- testScript.sh
+++ testScript.sh
@@ -3,11 +3,18 @@
 files=$(ls ./assignment3-tests-and-results/ | grep '[a-zA-Z]\.txt')
 
 outputname=project3
-
+outputfolder=results
 g++ $1 $2 -o $outputname ./template-rt.cpp  
+
+if [ ! -d "$outputfolder" ]; then
+	mkdir $outputfolder
+fi
+
 
 for file in $files 
 do
 	./$outputname './assignment3-tests-and-results/'$file
 done
+
+mv *.ppm ./$outputfolder
 
